53221539bec4c2cd3977279a5006cbfe0991173c
Mazdak <nazemi.works@gmail.com>
2020-02-13 07:18:23 +0330

    starter naturous

--- first/index.js
+++ first/index.js
@@ -1,41 +1,24 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
-
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+// slug is last part of url that contain string the resuerce product/freshavacado
 // ===============================================================
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  } else {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "");
-  }
-  return output;
-};
+const replaceTemplate = require('./module/replaceTemplate');
+
 // ===============================================================
 // Sync files read
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+const slugsObj = dataObj.map(el => slugify(el.productName, { lower: true }));
 
-const templateOverview = fs.readFileSync(
-  `${__dirname}/templates/overview.html`,
-  "utf-8"
-);
+const templateOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const templateCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
-const templateProduct = fs.readFileSync(
-  `${__dirname}/templates/product.html`,
-  "utf-8"
-);
+
+const templateProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 // =================================================================
 const server = http.createServer((req, res) => {
   // console.log(req.url);
@@ -44,31 +27,33 @@ const server = http.createServer((req, res) => {
   // true parse query to object
   const { query, pathname } = url.parse(req.url, true);
   // console.log(query.id, pathname);
-  // OVERVIEW PAGE -----------------------------------------------------
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, {
-      "Content-type": "text/html"
-    });
-    const cardsHtml = dataObj
-      .map(el => replaceTemplate(templateCard, el))
-      .join("");
 
-    const output = templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+  // OVERVIEW PAGE -----------------------------------------------------
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join('');
+    const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
     res.end(output);
   }
   // PRODUCT PAGE -----------------------------------------------------
-  else if (pathname === "/product") {
+  else if (pathname === '/product') {
     res.writeHead(200, {
-      "Content-type": "text/html"
+      'Content-type': 'text/html'
     });
+
     const product = dataObj[query.id];
     const output = replaceTemplate(templateProduct, product);
+
     res.end(output);
   }
   // API PAGE -----------------------------------------------------------
-  else if (pathname === "/api") {
+  else if (pathname === '/api') {
     res.writeHead(200, {
-      "Content-type": "application/json"
+      'Content-type': 'application/json'
     });
     res.end(data);
   }
@@ -76,13 +61,13 @@ const server = http.createServer((req, res) => {
   else {
     // also can send headers as obj
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "hellow-world"
+      'Content-type': 'text/html',
+      'my-own-header': 'hellow-world'
     });
-    res.end("<h1>that is bad page, not found 404!</h1>");
+    res.end('<h1>that is bad page, not found 404!</h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Server listening on 8000!");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Server listening on 8000!');
 });
